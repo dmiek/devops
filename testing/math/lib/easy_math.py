4a680f5ed2b23448d9511563d92eb7a3ded9eb93
dmiek <michaelnilssondesigns@live.com>
2020-09-30 14:35:25 +0200

    adding paranthesis to print statements

--- testing/math/lib/easy_math.py
+++ testing/math/lib/easy_math.py
@@ -1,11 +1,11 @@
 import operator as op
 
 def setup():
-    print "setting stuff up"
+    print("setting stuff up")
 
 def calculate(number1, number2, operation):
     arithmetic_function = { '+': op.add, '-': op.sub, '*': op.mul, '/': op.div }
     return arithmetic_function[operation](number1, number2)
 
 def teardown():
-    print "tearing stuff down"
+    print("tearing stuff down")
