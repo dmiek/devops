904825a18f892fdda29ca44c421d74d2d28100e5
dmiek <michaelnilssondesigns@live.com>
2020-10-09 22:27:35 +0200

    converting tests to BDD.

--- idioten/feature_idioten_deck.feature
+++ idioten/feature_idioten_deck.feature
@@ -2,16 +2,16 @@ Feature: Deck
     The Deck is the foundation of the game.
 
     Scenario: Deck only contains allowed colours
-        Given new round started
-        When new deck is generated
+        Given no active round
+        When new deck generated
         Then deck contains only allowed colours
 
     Scenario: Deck only contains allowed ranks
-        Given new round started
-        When new deck is generated
+        Given no active round
+        When new deck generated
         Then deck contains only allowed ranks
 
     Scenario: Deck is shuffled again at the start of each round
         Given no active round
-        When new round started
-        Then deck is shuffled
+        When new deck generated
+        Then new deck is different from previous deck
--- idioten/test_idioten_deck.py
+++ idioten/test_idioten_deck.py
@@ -1,8 +1,4 @@
-import pytest
-from pytest_bdd import scenario, given, when, then
-
-from idioten_board import new_game
-from idioten_board import game_active
+from pytest_bdd import scenarion, given, when, then
 from idioten_deck import create_deck
 
 game_active = False
@@ -13,25 +9,28 @@ def test_create_deck():
 
 @given('no active round')
 def no_active_round():
-    assert.game_active = False
+    previous_deck = create_deck()
 
-@when('new deck is generated')
-def new_round_started():
-    create_deck()
-
+@when('new deck generated')
+def new_round_generated():
+    new_deck = create_deck()
 
 @then('deck contains only allowed colours')
-def test_create_deck_allowed_colour(self):
-        # Check that deck only contains allowed colours.
-        deck = create_deck()
-        allowed_colour = ["D","S","C","H"]
-        for i in deck:
-            self.assertIn(i[1], allowed_colour)
+def test_create_deck_allowed_colour():
+    # Check that deck only contains allowed colours.
+    new_deck = create_deck()
+    allowed_colour = ["D","S","C","H"]
+    for i in new_deck:
+        assert i[1] in allowed_colour
 
 @then('Deck contains only allowed ranks')
-def test_create_deck_allowed_ranks(self):
-        # Check that deck contains only allowed ranks.
-        deck = create_deck()
-        allowed_ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
-        for i in deck:
-            self.assertIn(i[0], allowed_ranks)
+def test_create_deck_allowed_ranks():
+    # Check that deck contains only allowed ranks.
+    new_deck = create_deck()
+    allowed_ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
+    for i in new_deck:
+        assert i[0] in allowed_ranks
+
+@then('new deck is different from previous deck')
+def test_new_deck_previous_deck():
+    assert new_deck != previous_deck
