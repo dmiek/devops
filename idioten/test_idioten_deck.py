36ee96b5aa873f266acc576518b224c99adc0c54
dmiek <michaelnilssondesigns@live.com>
2020-10-10 13:56:35 +0200

    refactoring

--- idioten/test_idioten_deck.py
+++ idioten/test_idioten_deck.py
@@ -1,31 +1,34 @@
 from pytest_bdd import scenario, given, when, then
 from idioten_deck import create_deck
 
+deck = []
+previous_deck = create_deck()
+
+
 @scenario('feature_idioten_deck.feature', 'New deck is different from previous one')
 def test_create_deck():
     pass
 
-@given('no deck')
-def no_deck():
-    deck = []
-    assert type(deck) == list
 
 @given('existing deck')
 def existing_deck():
-    previous_deck = create_deck()
     assert len(previous_deck) == 52
+
 
 @when('deck shuffled')
 def new_deck_generated():
-    new_deck = create_deck()
-    assert type(new_deck) == list
+    deck = create_deck()
+    assert type(deck) == list
+    return deck
+
 
 @then('deck contains only allowed colours')
 def test_create_deck_allowed_colour():
     # Check that deck only contains allowed colours.
     allowed_colour = ["D", "S", "C", "H"]
-    for i in new_deck:
+    for i in deck:
         assert i[1] in allowed_colour
+
 
 @then('deck contains only allowed ranks')
 def test_create_deck_allowed_ranks():
@@ -34,15 +37,18 @@ def test_create_deck_allowed_ranks():
     for i in new_deck:
         assert i[0] in allowed_ranks
 
+
 @then('new deck is different from previous deck')
 def test_new_deck_previous_deck():
     previous_deck = create_deck()
     new_deck = create_deck()
     assert new_deck != previous_deck
 
+
 @then('deck is of the correct type')
 def test_deck_type():
     assert type(new_deck) == list
+
 
 @then('deck contains no duplications')
 def test_unique_card():
