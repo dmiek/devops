0f0ca630e397f08e63a48ef68770574d8a710dc7
dmiek <michaelnilssondesigns@live.com>
2020-10-14 11:54:09 +0200

    Refactored kb_input() and corrected linting.

--- idioten/idioten_input.py
+++ idioten/idioten_input.py
@@ -1,26 +1,34 @@
 """
 Module for handling game input.
 """
+import sys
 
+INPUT = ()
 VALID_INPUT = ('d', '1', '2', '3', '4', 'n', 'e')
+GAME_ACTIVE = 1
 
-def kb_input():
+
+def kb_input(game_active):
     """Keyboard input."""
-    print("Select action: \n1-4 = remove card in column \nd = deal cards\nn = new game ")
+    if game_active == 0:
+        sys.exit('Exiting program; no game active.')
 
+    print("Select Action: \n1-4 = Remove Card in Column \nd = Deal Cards\nn = New Game ")
     inp = input()
     print(inp)
 
     if inp == "d":
-        print("dealing cards")
+        print("Dealing Cards")
     elif inp in ('1', '2', '3', '4'):
-        print("removing card in column")
+        print("Removing Card in Column " + inp)
     elif inp == "n":
-        print("dealing new game")
+        print("Dealing New Game")
     elif inp == 'e':
-        print('exiting')
+        print('Player exiting program')
     else:
-        print("input not mapped to action")
+        print("Input Not Mapped to Action")
+    return inp
 
 
-#while inp != kb_input()
+while INPUT != 'e':
+    INPUT = kb_input(GAME_ACTIVE)
