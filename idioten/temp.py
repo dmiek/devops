290e3c96d1d91d7af1a78807cac64a8cca864f49
dmiek <michaelnilssondesigns@live.com>
2020-10-19 22:21:34 +0200

    files for testing "class".

---
+++ idioten/temp.py
@@ -0,0 +1,25 @@
+import pytest
+from idioten_deck import create_deck
+
+
+
+# class Decks:
+#     """dwdwadaw"""
+#
+#     def generate_deck():
+#         deck = create_deck()
+#         return deck
+
+decks = {}
+
+def generate_decks(d):
+    """ Fixture for generating the card decks. """
+    d["new"] = create_deck()
+    d["old"] = create_deck()
+    return d
+
+
+decks = generate_decks(decks)
+print(decks)
+print(decks["new"])
+print(decks["old"])
--- testing/tests/klass.py
+++ testing/tests/klass.py
@@ -1,4 +1,4 @@
-class Test():
+class Test:
 
     def addition(self, x):
         return x + x
