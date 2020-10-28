b1de87969c278408f465789bd1143ed2c02323e0
dmiek <michael.nilsson@infotiv.se>
2020-10-28 19:33:27 +0100

    commented out code to be used for later

--- idioten/application/idioten_input.py
+++ idioten/application/idioten_input.py
@@ -23,35 +23,35 @@ def kb_input():
         return INPUTS
 
 
-def nput(game_active):
-    """
-    Keyboard input.
-    """
-    if game_active == 0:
-        print("Game over. Start a new game.")
-    else:
-        print("Select action: \n1-4 = Remove card in column \nd = Deal cards\nn = New game")
-
-    inp = input()
-    print(inp)
-
-
-    if inp != 'n':
-        if game_active == 0:
-            print('Game not running')
-        else:
-            print("Dealing new game")
-
-    elif inp == "d":
-        print("Dealing cards")
-    elif inp in ('1', '2', '3', '4'):
-        print("Removing card in column " + inp)
-
-    elif inp == 'e':
-        sys.exit('Player exiting program.')
-    else:
-        print("Input not mapped to action")
-    return inp
+# def nput(game_active):
+#     """
+#     Keyboard input.
+#     """
+#     if game_active == 0:
+#         print("Game over. Start a new game.")
+#     else:
+#         print("Select action: \n1-4 = Remove card in column \nd = Deal cards\nn = New game")
+#
+#     inp = input()
+#     print(inp)
+#
+#
+#     if inp != 'n':
+#         if game_active == 0:
+#             print('Game not running')
+#         else:
+#             print("Dealing new game")
+#
+#     elif inp == "d":
+#         print("Dealing cards")
+#     elif inp in ('1', '2', '3', '4'):
+#         print("Removing card in column " + inp)
+#
+#     elif inp == 'e':
+#         sys.exit('Player exiting program.')
+#     else:
+#         print("Input not mapped to action")
+#     return inp
 
 
 # t = Input('test input', 'unknown')
