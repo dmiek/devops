1e3ab700e4f7553ce923d8b01315b43793956dd2
dmiek <michael.nilsson@infotiv.se>
2020-10-28 19:29:50 +0100

    removed unused fixtures

--- idioten/tests/conftest.py
+++ idioten/tests/conftest.py
@@ -17,20 +17,6 @@ def decks():
 
 
 @pytest.fixture
-def accepted_values(valid_input):
-    """ Fixture for allowed values. """
-    allowed_input = valid_input
-    return allowed_input
-
-
-@pytest.fixture
-def rejected_values(invalid_input):
-    """ Fixture for allowed values. """
-    rejected_input = invalid_input
-    return rejected_input
-
-
-@pytest.fixture
 def input_fixture():
     """ Fixture for creating input object. """
     inputs = {"keyboard": '', 'status': 'unknown'}
