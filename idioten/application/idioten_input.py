2b500a7118d55d0a69e8ce58f4ecf55b3143c0d9
dmiek <michaelnilssondesigns@live.com>
2020-11-16 13:40:49 +0100

    removed old crap

--- idioten/application/idioten_input.py
+++ idioten/application/idioten_input.py
@@ -1,7 +1,6 @@
 """
 Module for handling game input.
 """
-import sys
 
 VALID_INPUT = ('d', '1', '2', '3', '4', 'n', 'e', 'hello')
 INPUTS = {"keyboard": '', "status": 'unknown'}
@@ -17,45 +16,7 @@ def kb_input():
         INPUTS["status"] = 'OK'
         print(INPUTS)
         return INPUTS
-    else:
-        INPUTS["status"] = 'NOK'
-        print(INPUTS)
-        return INPUTS
 
-
-# def nput(game_active):
-#     """
-#     Keyboard input.
-#     """
-#     if game_active == 0:
-#         print("Game over. Start a new game.")
-#     else:
-#         print("Select action: \n1-4 = Remove card in column \nd = Deal cards\nn = New game")
-#
-#     inp = input()
-#     print(inp)
-#
-#
-#     if inp != 'n':
-#         if game_active == 0:
-#             print('Game not running')
-#         else:
-#             print("Dealing new game")
-#
-#     elif inp == "d":
-#         print("Dealing cards")
-#     elif inp in ('1', '2', '3', '4'):
-#         print("Removing card in column " + inp)
-#
-#     elif inp == 'e':
-#         sys.exit('Player exiting program.')
-#     else:
-#         print("Input not mapped to action")
-#     return inp
-
-
-# t = Input('test input', 'unknown')
-# print(t.keyboard)
-# print(t.status)
-
-#kb_input()
+    INPUTS["status"] = 'NOK'
+    print(INPUTS)
+    return INPUTS
