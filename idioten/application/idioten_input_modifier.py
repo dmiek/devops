99a0e100c0aff3e65903b38d87a72d4705a91ffb
dmiek <michaelnilssondesigns@live.com>
2020-11-16 10:38:02 +0100

    Function that translates input to logic variables

---
+++ idioten/application/idioten_input_modifier.py
@@ -0,0 +1,6 @@
+""" Module that modifies the keyboard input to programming logic. """
+
+def input_modifier(given_input):
+    """ Converts string input to integers and positional logic. """
+    given_input = int(given_input) - 1
+    return given_input
