364e2c440c73a152c22f1bd7f76c00ee04bfe572
dmiek <michael.nilsson@infotiv.se>
2021-03-20 11:18:06 +0100

    started to develop game flow

--- idioten/application/idioten_game.py
+++ idioten/application/idioten_game.py
@@ -1,27 +1,40 @@
 """
 Idioten card game.
 """
-# import sys
-#
-#
-# def show_frontpage():
-#     print("Game not active. What would you like to do? \n Press 'n' to play game \n Press 'e' to exit")
-#     r = input()
-#     if r == 'e':
-#         sys.exit('Thank you for playing! Bye!')
-#
-#
-#
-# while GAME_ACTIVE == 0:
-#
-#
-# show_frontpage()
-#
-#
-# def play_game():
-#     """ Call to start game """
-#     show_frontpage()
+
+
+from idioten_setup import game_setup
+from idioten_input import kb_input
+
+game_components = {}
+
+print('*** GAME STARTED ***')
+print('*** What do you want to do? ***')
+print('*** Press "N" to start a new game. ***')
+print('*** Type "help" to display HELP menu. ***')
+game_input = kb_input()
+print(game_input)
 
 
 
 
+
+def determine_input(game_input):
+    """ Module for determining what actions is requested. """
+
+
+def start_new_game(game_components):
+    """ Sets up a new game. """
+    game_components = game_setup(game_components)
+    return game_components
+
+
+def helpMenu():
+    print("*** HELP MENU ***")
+    print("d = deal cards")
+    print("e = end game")
+    print("m = move card to empty spot")
+    print("n = new game")
+    print("t = test mode")
+    print("1-4 = remove card in column")
+    print("*** END HELP MENU ***")
