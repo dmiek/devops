4d4d696be49e4c397703c64d056ac6264240035b
dmiek <michael.nilsson@infotiv.se>
2021-03-20 10:01:41 +0100

    refactored to return a dict of game components for continued gameplay use

--- idioten/application/idioten_setup.py
+++ idioten/application/idioten_setup.py
@@ -4,19 +4,17 @@ Module for setting up a new game.
 from idioten.application.idioten_empty_row import empty_row
 from idioten.application.idioten_deck import create_deck
 
-deck = []
-board = []
-rond = []
 
-def game_setup():
+
+def game_setup(game_components):
     """ Setting up new game from scratch. """
     print("***")
     print("*** SETTING UP GAME ***")
-    board = [empty_row()]
+    game_components["board"] = [empty_row()]
     print("empty board created")
     #rond = empty_row()
     #print("round emptied")
     #deck = create_deck()
     print("*** SETUP FINISHED ***")
     #return deck, board, rond
-    return board
+    return game_components
