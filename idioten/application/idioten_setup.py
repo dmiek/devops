c9380b85197c288f8ddf5768ff908d9562f3d530
dmiek <michael.nilsson@infotiv.se>
2021-03-20 10:49:34 +0100

    added functionality for setting up deck

--- idioten/application/idioten_setup.py
+++ idioten/application/idioten_setup.py
@@ -5,16 +5,13 @@ from idioten.application.idioten_empty_row import empty_row
 from idioten.application.idioten_deck import create_deck
 
 
-
 def game_setup(game_components):
     """ Setting up new game from scratch. """
     print("***")
     print("*** SETTING UP GAME ***")
     game_components["board"] = [empty_row()]
-    print("empty board created")
-    #rond = empty_row()
-    #print("round emptied")
-    #deck = create_deck()
+    print("board cleared")
+    game_components["deck"] = create_deck()
+    print("deck shuffled")
     print("*** SETUP FINISHED ***")
-    #return deck, board, rond
     return game_components
