0564ee0aab6411cf6ab1fe7858e92f3f672eb653
dmiek <michael.nilsson@infotiv.se>
2021-03-20 16:00:16 +0100

    refactored input function

--- idioten/application/idioten_input.py
+++ idioten/application/idioten_input.py
@@ -11,12 +11,22 @@ def kb_input():
     Method for handling game input through keyboard.
     Will only accept values defined in VALID_INPUT.
     """
-    INPUTS["keyboard"] = input('Waiting for input')
+    INPUTS["keyboard"] = input('Waiting for input\n')
     if INPUTS["keyboard"] in VALID_INPUT:
         INPUTS["status"] = 'OK'
-        print(INPUTS)
-        return INPUTS
-
-    INPUTS["status"] = 'NOK'
+    else:
+        INPUTS["status"] = 'NOK'
     print(INPUTS)
     return INPUTS
+
+
+def valid_input():
+    """ Returns only valid input. """
+    ok_input = {}
+    ok_input["status"] = 'NOK'
+    while ok_input["status"] == 'NOK':
+        ok_input = kb_input()
+        if ok_input["status"] == 'NOK':
+            print('Input a valid value.')
+        else:
+            return ok_input["keyboard"]
