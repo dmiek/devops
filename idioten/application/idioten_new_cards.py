c7e5d50a4c129aab321d2bd97818c3f5042d7c5d
dmiek <michael.nilsson@infotiv.se>
2021-03-21 13:27:56 +0100

    added functionality for adding cards to empty rows

--- idioten/application/idioten_new_cards.py
+++ idioten/application/idioten_new_cards.py
@@ -15,5 +15,9 @@ def new_cards(game_comps):
 def deal_cards(game_comps):
     """ Deal cards to the board from the deck. """
     game_comps = new_cards(game_comps)
+    for i in range(len(game_comps["board"][0])):
+        if game_comps["board"][-1][i] == '- ':
+            game_comps["board"][-1][i] = game_comps["new_round"][i]
+            game_comps["new_round"][i] = '- '
     game_comps["board"].append(game_comps["new_round"])
     game_comps["new_round"] = []
